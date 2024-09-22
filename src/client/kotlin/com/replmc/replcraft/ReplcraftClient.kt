1ed530e72164eea5784afe541f6d2a961753a543
SkyfallWasTaken <mahadkalam1@proton.me>
2024-09-22 11:28:00 +0100

    first commit

---
+++ src/client/kotlin/com/replmc/replcraft/ReplcraftClient.kt
@@ -0,0 +1,9 @@
+package com.replmc.replcraft
+
+import net.fabricmc.api.ClientModInitializer
+
+object ReplcraftClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
