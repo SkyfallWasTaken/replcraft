ac62251a4096469d4180a706f2707fd3065e65f5
Mahad Kalam <mahadkalam1@proton.me>
2024-09-22 12:17:50 +0000

    Remove client-side code

--- src/client/kotlin/com/replmc/replcraft/ReplcraftClient.kt
+++
@@ -1,9 +0,0 @@
-package com.replmc.replcraft
-
-import net.fabricmc.api.ClientModInitializer
-
-object ReplcraftClient : ClientModInitializer {
-	override fun onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
-}
